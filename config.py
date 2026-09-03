f93a7e7b173e2dbb23a8b12e43844f1dacdf5bc4
N Varshith Goud <625058@student.nitandhra.ac.in>
2026-09-03 17:19:06 +0530

    Ready for deployment

--- config.py
+++ config.py
@@ -1,19 +1,6 @@
 import os
 
-
-
-SECRET_KEY = "studentportal123"
-
-
-
-DATABASE_URL = os.getenv(
-
-    "DATABASE_URL",
-
-    "postgresql://capacity_connect_user:oZyUBeTi0XoDF0G8GIbyhqRs8xUw0Bn1@dpg-dacj9muq1p3s738cfl6g-a.singapore-postgres.render.com/capacity_connect"
-
-)
-
-
+SECRET_KEY = os.getenv("SECRET_KEY", "studentportal123")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 DEBUG = True
